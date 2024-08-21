61a4d9238df5142fa21afb8f9f6abcd012a6990b
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-08-21 01:19:50 -0400

    fix: check-in bin/radashi-helper.js

---
+++ packages/radashi-helper/bin/radashi-helper.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+import('../dist/cli.js').then(async ({ run, setPromptHandler, setLogHandler, setStdio, EarlyExitError, RadashiError }) => {
+  setLogHandler(log)
+  setPromptHandler(prompt)
+  setStdio('inherit')
+
+  try {
+    await run(process.argv)
+  } catch (error) {
+    if (error instanceof EarlyExitError) {
+      process.exit(0)
+    }
+    if (error instanceof RadashiError) {
+      console.error(error.message)
+    } else {
+      console.error(error)
+    }
+    process.exit(1)
+  }
+})
+
+function log(type, msg, ...args) {
+  switch(type) {
+    case 'info':
+      console.log(msg, ...args)
+      break
+    case 'warn':
+      console.warn(yellow('ATTN') + ' ' + msg, ...args)
+      break
+    case 'error':
+      console.error(msg, ...args)
+      break
+  }
+}
+
+async function prompt(options) {
+  const { default: prompts } = await import('prompts')
+
+  // Print a blank line to separate the prompt from the output.
+  console.log()
+  
+  const answer = await prompts(options)
+  return answer[options.name]
+}
