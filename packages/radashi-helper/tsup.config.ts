f4c3f99f56d3638493154d7aabe29c4b1b6f9f54
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-08-22 02:41:32 -0400

    chore: enable CJS build output
    Required for radashi-vscode to import this package, since VSCode runs extensions with an older NodeJS version where dynamic imports are not allowed.

--- packages/radashi-helper/tsup.config.ts
+++ packages/radashi-helper/tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/cli.ts', 'src/vitest/plugin.ts', 'src/esbuild/plugin.ts'],
-  format: ['esm'],
+  format: ['esm', 'cjs'],
   splitting: true,
   dts: true,
 })
