7b2e88c243fda9a4a54c3c9f989552a0d8b05de4
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-11-09 16:27:24 -0500

    chore: remove unimplemented tests

--- packages/radashi-helper/test/format.test.ts
+++
@@ -1,7 +0,0 @@
-import { expectForkSnapshot } from './util/mocks'
-import { run } from './util/run'
-
-test.skip('format', async () => {
-  await run('format')
-  await expectForkSnapshot()
-})
--- packages/radashi-helper/test/pr-create.test.ts
+++
@@ -1,7 +0,0 @@
-import { expectForkSnapshot } from './util/mocks'
-import { run } from './util/run'
-
-test.skip('create a pr branch', async () => {
-  await run('pr create --dry-run')
-  await expectForkSnapshot()
-})
